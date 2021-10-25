4e8a98ad2e112ffc59b805408c902856951997aa
Octavio Garcia <octaviogarciapc@gmail.com>
2021-10-24 20:12:09 -0500

    content layoout

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
-import "./navbar.css"
+import React from "react";
+import "./navbar.css";
 
 export default function NavBar() {
-    return (
-        <div className='navBarContainer'>
-            <div className="brand">dillo records</div>
+  return (
+    <div className="navBarContainer">
+      <div className="brand">dillo records</div>
+      <div className="navigation">
+        <div className="upperNavigation">
+          <div className="artist">Artist</div>
+          <div className="media">Media</div>
+          <div className="shop">Shop</div>
         </div>
-    )
+
+        <div className="lowernavigation">
+          <div className="about">About</div>
+          <div className="contact">Contact</div>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/components/right/Right.jsx
+++ src/components/right/Right.jsx
@@ -1,6 +1,14 @@
 import React from "react";
 import "./right.css";
+import { GiShoppingCart } from "react-icons/gi";
 
 export default function Right() {
-  return <div className="rightcontainer"> cart</div>;
+  return (
+    <div className="rightcontainer">
+      <div className="cartContainer">
+        <div className="cartTotal">2</div>
+        <GiShoppingCart />
+      </div>
+    </div>
+  );
 }
